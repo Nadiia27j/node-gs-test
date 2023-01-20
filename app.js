2c8c629d82e2438c291a99da8909d103baa96860
Надія Ткач <nadiya77723@gmail.com>
2023-01-20 20:43:08 +0200

    //

--- app.js
+++ app.js
@@ -8,4 +8,23 @@
 // require('') vs import
 
 // require('./calculator');
-require('./OOP');
+// require('./OOP');
+
+
+const { request, response } = require('express');
+const express = require('express');
+const server = express();
+// console.log(server);
+
+server.get('/contacts', (request, response) => {
+    // console.log(request.url);
+    // console.log(request.method);
+    // console.log(request.headers);
+    response.send('<h2>Contacts page<h2>');
+})
+
+server.get('/', (request, response) => {
+    response.send('<h2>Home page<h2>')
+})
+
+server.listen(3000, () => console.log("Server running"));
